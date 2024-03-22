17b9d5f14b36934e0d40bad5345fed7732aad3cf
Uendi Hoxholli <uendihoxholli33@gmail.com>
2024-03-22 13:16:05 +0100

    feat: first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Testing Remix</h1>
+      <a href='/news'>Go to news page</a>
+    </div>
+  );
+}
